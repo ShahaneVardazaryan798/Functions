6a89bd6ef1bebe2469d68767a3c8c5996148202a
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-05-02 01:18:15 +0400

    first func

---
+++ LessonAoutFunc/LessonAoutFunc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace LessonAoutFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter the Num you want");
+            int n;
+            CountSqrt(n=int.Parse(ReadLine()));
+        }
+        public static int CountSqrt(int n)
+        {
+            int result=0;
+            for(int i=1;i<n;i++)
+            {
+
+                result = i * i;
+                WriteLine($"{i}-i qarakusin ="+ result); 
+            }
+            return result;
+        }
+
+    }
+}
