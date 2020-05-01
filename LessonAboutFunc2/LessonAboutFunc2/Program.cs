d141b0ce1675101e21485d0383d496c655d7d885
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-05-02 01:28:01 +0400

    second func

---
+++ LessonAboutFunc2/LessonAboutFunc2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LessonAboutFunc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter the number and the persent");
+            int a, b;
+            CountPersent(a = int.Parse(ReadLine()), b = int.Parse(ReadLine()));
+        }
+        public static int CountPersent(int a, int b)
+        {
+            int x = (a * b) / 100;
+            WriteLine($"The {b} persent of {a} = " + x);
+            return x;
+        }
+    }
+}
