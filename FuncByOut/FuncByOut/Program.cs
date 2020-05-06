d47ccf6001c2b5cfc0c33f23e0a7e3f963ca4ab0
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-05-07 01:42:22 +0400

    12345

---
+++ FuncByOut/FuncByOut/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FuncByOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] b;
+            Integer(out  b);  
+        }
+        public static int Integer(out int[]a)
+        {
+            Random rd = new Random();
+            a = new int[10];
+            for (int i = 0; i <a.Length; i++)
+            {
+                a[i] = rd.Next(-100, 100);
+                Write(a[i] + "\t");
+            }
+            int max=-1;
+            for (int i = 0; i <a.Length; i++)
+            {
+                if(a[i]>max)
+                {
+                    max = a[i];
+                }
+            }
+            WriteLine("\n Max = " + max);
+            int min = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(a[i]<min)
+                {
+                    min = a[i];
+                }
+            }
+            WriteLine("Min = " + min);
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            WriteLine("Sum = " + sum);
+            WriteLine("Midlle value = " + sum / 10);
+            int middle = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(a[i]<0)
+                {
+                    sum += a[i];
+                    middle = sum / i;
+                }
+            }
+            WriteLine("The negative sum = " + sum);
+            WriteLine("The middle of negative nums = " + middle);
+
+            return 0;
+        }
+    }
+}
---
+++ LessonAboutFunc3/LessonAboutFunc3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LessonAboutFunc3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            CountAreaOfRect(a = int.Parse(ReadLine()), b = int.Parse(ReadLine()));
+        }
+        public static double CountAreaOfRect(int a,int b)
+        {
+            int result=0;
+            string s = "*";
+            for(int i=0;i<a-1;i++)
+            {
+                Write(s);
+            }
+            for(int i=1; i>b; i++)
+            {
+                WriteLine(s);
+            }
+            for (int i = 0; i < a - 1; i++)
+            {
+                Write(s);
+            }
+                         
+
+             
+            return result;
+        } 
+            
+            
+           
+    }
+}
