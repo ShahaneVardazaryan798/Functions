249c0887a02965a20f53111a745df434eee313f2
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-05-08 01:40:34 +0400

    end

---
+++ FuncByOut2/FuncByOut2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FuncByOut2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] b;
+            int x;
+            CountByParameter(out b, out x);
+        }
+        public static int CountByParameter(out int[]arr,out int n)
+        {
+            Random rd = new Random();
+            WriteLine("Enter the num");
+            n = int.Parse(ReadLine());
+            arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rd.Next(-100, 100);
+                Write("Arr = " + arr[i]+"\t");
+            }
+            
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i]==n)
+                {
+                    count = 0;
+                    count++;
+                }
+                
+            }
+            WriteLine("Count = " + count);
+            int index = 0;
+            for (int i = 0; i<arr.Length; i++)
+            {
+                if(arr[i]==n)
+                {
+                    index = i;
+                   
+                }
+                else
+                {
+                    index = -1;
+                }
+
+            }
+            WriteLine("Index = " + index);
+
+            return index;
+        }
+    }
+}
